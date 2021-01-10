88de826199921359029241c129b60b12a7f1e0bf
zharikovartem <42375825+zharikovartem@users.noreply.github.com>
2021-01-10 23:03:17 +0300

    Init

---
+++ src/Components/NewTaskForm.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react'
+import moment from 'moment'
+import { Form, Input, Button, Checkbox, TimePicker } from 'antd'
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+}
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+}
+
+type PropsType = {
+    selectedDate: moment.Moment
+}
+
+const { TextArea } = Input;
+
+const timeFormat: string = 'HH:mm';
+
+const NewTaskForm: React.FC<PropsType> = () => {
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    const onTimeChange = (value: moment.Moment | null, dateString: string):void => {
+        console.log(value, dateString);
+    }
+
+    return (
+        <Form
+            {...layout}
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+        >
+            <Form.Item
+                label="Task name"
+                name="taskNime"
+                rules={[{ required: true, message: 'Please input task name!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Task time"
+                name="taskTime"
+                rules={[{ required: true, message: 'Please input time!' }]}
+            >
+                <TimePicker 
+                    onChange={onTimeChange} 
+                    // defaultValue={moment('12:08', timeFormat)} 
+                    format={timeFormat} 
+                />
+            </Form.Item>
+
+            <Form.Item
+                label="Description"
+                name="description"
+                // rules={[{ required: false, message: 'Please input your password!' }]}
+            >
+                <TextArea rows={2} />
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                    Create
+                </Button>
+            </Form.Item>
+        </Form>
+    )
+}
+
+export default NewTaskForm
---
+++ src/Components/ToDoList.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import { DatePicker, Card, Divider, Button, Drawer } from 'antd'
+import { FileAddOutlined } from '@ant-design/icons'
+import moment from 'moment'
+import NewTaskForm from './NewTaskForm'
+
+const ToDoList: React.FC = () => {
+    const [selectedDate, setselectedDate] = useState<moment.Moment>(moment())
+    const [visible, setVisible] = useState(false)
+    const [isAddActive, setIsAddActive] = useState(false)
+
+    const onDateChange = (value: moment.Moment | null, dateString: string): void => {
+        if (value !== null) {
+            setselectedDate(value)
+            setIsAddActive(false)
+        } else {
+            setselectedDate(moment(null))
+            setIsAddActive(true)
+        }
+    }
+
+    const showDrawer = ():void => {
+        setVisible(true);
+      }
+
+      const onClose = ():void => {
+        setVisible(false);
+      }
+
+    type timeScaleItemType = React.ReactElement<string>
+    const timeScale = (): Array<timeScaleItemType> => {
+        let timeScaleArrey: Array<timeScaleItemType> = []
+        for (let index: number = 0; index < 24; index++) {
+
+            timeScaleArrey.push(
+                <Divider key={index} orientation="left">
+                    {index <= 9 ? '0' : null}{index}:00
+                </Divider>)
+        }
+        return timeScaleArrey
+    }
+
+    return (
+        <>
+            <div className="site-card-border-less-wrapper">
+                <Card
+                    title={
+                        <>
+                            <label>Select date:</label>
+                            <DatePicker
+                                onChange={onDateChange}
+                                defaultValue={selectedDate}
+                                format='DD-MM-YYYY'
+                                style={{ marginLeft: 10 }}
+                            />
+                            <Button 
+                                type="primary" 
+                                shape="round" 
+                                icon={<FileAddOutlined />} 
+                                style={{ marginLeft: 10 }} 
+                                size="small" 
+                                onClick={showDrawer}
+                                disabled={isAddActive}
+                            >
+                                Add
+                            </Button>
+                        </>
+                    }
+                    bordered={false}
+                >
+                    {timeScale()}
+                </Card>
+
+                <Drawer
+                    title={"Create New Task for " + selectedDate.format('DD MMM YYYY')}
+                    placement="right"
+                    closable={false}
+                    onClose={onClose}
+                    visible={visible}
+                    // width="80%"
+                >
+                    <NewTaskForm selectedDate={selectedDate}/>
+                </Drawer>
+            </div>
+
+        </>
+    )
+}
+
+export default ToDoList
